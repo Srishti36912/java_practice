c2b653b912a3b91e3cfaa44dad38754cb987d928
Srishti Tripathi <shrishtitripathi43@gmail.com>
2025-07-17 15:45:52 +0530

    printing name and phone number using hashmap

---
+++ easy/map.java
@@ -0,0 +1,30 @@
+package easy;
+
+import java.util.*;
+import java.io.*;
+
+class map{
+	public static void main(String []argh)
+	{
+		Scanner in = new Scanner(System.in);
+        HashMap<String, String> phoneNumber = new HashMap<>();
+		int n=in.nextInt();
+		in.nextLine();
+		for(int i=0;i<n;i++)
+		{
+			String name=in.nextLine();
+			String phone=in.nextLine();
+			phoneNumber.put(name, phone);    
+		}
+		while(in.hasNext())
+		{
+			String s=in.nextLine();
+            if(phoneNumber.containsKey(s)) {
+                System.out.println(s + "=" + phoneNumber.get(s));
+            }
+            else {
+                System.out.println("Not found");
+            }
+		}
+	}
+}
