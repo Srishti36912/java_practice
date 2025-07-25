bc8f51ad7a04a3ec1992c8aed5b174a44d1e96a0
Srishti Tripathi <shrishtitripathi43@gmail.com>
2025-07-25 13:44:34 +0530

    removed errors

--- easy/bigInteger.java
+++ easy/bigInteger.java
@@ -1,13 +1,9 @@
 package easy;
 
-import java.io.*;
 import java.util.*;
-import java.text.*;
 import java.math.*;
-import java.util.regex.*;
 
 public class bigInteger {
-
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         Scanner sc = new Scanner(System.in);
