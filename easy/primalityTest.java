d4de5383e7211897b21270e48be0bfb876f55867
Srishti Tripathi <shrishtitripathi43@gmail.com>
2025-08-14 20:04:09 +0530

    finding if the number is prime or not using BigInteger

---
+++ easy/primalityTest.java
@@ -0,0 +1,20 @@
+package easy;
+
+import java.io.*;
+import java.math.*;
+
+public class primalityTest {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String n = bufferedReader.readLine();
+        BigInteger num = new BigInteger(n);
+        if(num.isProbablePrime(100)) {
+            System.out.println("prime");
+        } else {
+            System.out.println("not prime");
+        }
+
+        bufferedReader.close();
+    }
+}
