1ba8e99bf1fcf9d0f7e0e5f8843c1c5126a65810
Srishti Tripathi <shrishtitripathi43@gmail.com>
2025-08-15 16:55:14 +0530

    completed some part of code and understood the usage of reflection

---
+++ easy/javaReflection.java
@@ -0,0 +1,37 @@
+package easy;
+import java.lang.reflect.Method;
+import java.util.*;
+
+class Student{
+    private String name;
+    private String id;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void anothermethod(){  }
+}
+
+public class javaReflection {
+    public static void main(String[] args){
+
+        Class student = new Student().getClass();
+        Method[] methods = student.getDeclaredMethods();
+
+        ArrayList<String> methodList = new ArrayList<>();
+        for(Method method: methods){
+            methodList.add(method.getName());
+        }
+        Collections.sort(methodList);
+        for(String name: methodList){
+            System.out.println(name);
+        }
+    }
+}
