9e5c9adda9d36a3e6a15f3fa140ff008459e5520
Srishti Tripathi <shrishtitripathi43@gmail.com>
2025-09-05 12:54:58 +0530

    extracting the content written in the tag

---
+++ medium/tagContentExtractor.java
@@ -0,0 +1,30 @@
+package medium;
+
+import java.util.*;
+import java.util.regex.*;
+
+public class tagContentExtractor{
+	public static void main(String[] args){
+		
+		Scanner in = new Scanner(System.in);
+		int testCases = Integer.parseInt(in.nextLine());
+		while(testCases>0){
+			String line = in.nextLine();
+			
+          	//Write your code here
+            boolean matchFound = false;
+            Pattern r = Pattern.compile("<(.+)>([^<]+)</\\1>");
+            Matcher m = r.matcher(line);
+            
+            while(m.find()) {
+                System.out.println(m.group(2));
+                matchFound = true;
+            }
+			
+            if(!matchFound) {
+                System.out.println("None");
+            }
+			testCases--;
+		}
+	}
+}
