299e0f28b66f77fc4c682efbb9c661d78f182c7c
Srishti Tripathi <shrishtitripathi43@gmail.com>
2025-09-26 20:09:03 +0530

    return true if you have won the game else return false

---
+++ medium/array1D.java
@@ -0,0 +1,39 @@
+package medium;
+
+import java.util.*;
+
+public class array1D {
+    
+    private static boolean canWin(int leap, int[] game, boolean[] visited, int pos) {
+ 
+        if(pos >= game.length) return true;
+        if(pos < 0 || game[pos] != 0 || visited[pos]) return false;
+        
+        visited[pos] = true;
+        
+        return canWin(leap, game, visited, pos-1) || 
+        canWin(leap, game, visited, pos+1) || canWin(leap, game, visited, pos+leap);
+    }
+    
+    public static boolean canWin(int leap, int[] game) {
+        // Return true if you can win the game; otherwise, return false.
+        return canWin(leap, game, new boolean[game.length], 0);
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int q = scan.nextInt();
+        while (q-- > 0) {
+            int n = scan.nextInt();
+            int leap = scan.nextInt();
+            
+            int[] game = new int[n];
+            for (int i = 0; i < n; i++) {
+                game[i] = scan.nextInt();
+            }
+
+            System.out.println( (canWin(leap, game)) ? "YES" : "NO" );
+        }
+        scan.close();
+    }
+}
